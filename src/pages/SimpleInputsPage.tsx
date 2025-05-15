642becb31d473c1714480e210474b605db08c3b8
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-05-15 17:16:41 +0300

    feat: new page for demonstrate new components (UnmaskedInput, Modal and ModalForms with lazy loading)

--- src/pages/SimpleInputsPage.tsx
+++ src/pages/SimpleInputsPage.tsx
@@ -1,23 +1,57 @@
-import { FormSimpleInputs } from "@/components/entities";
+import {
+  DeleteModal,
+  // FormSimpleInputs,
+  UserModalForm,
+} from "@/components/entities";
 import { Container } from "@/components/shared";
+import { ActionType } from "@/models/zodForms";
+import { Plus, Trash } from "lucide-react";
 
-export const SimpleInputsPage = () => {
-  const formData = {
-    name: "name",
-    firstName: "firstName",
-    lastName: "lastName",
-    email: "email@examle.com",
-    phone: "+373 00 00 00",
-    birthday: new Date(),
-  };
+export default function SimpleInputsPage() {
+  // const formData = {
+  //   name: "name",
+  //   firstName: "firstName",
+  //   lastName: "lastName",
+  //   email: "email@examle.com",
+  //   phone: "+373 00 00 00",
+  //   birthday: new Date(),
+  // };
   return (
     <section className="flex grow">
       <Container>
-        <div className="flex h-full flex-col items-center justify-center gap-0">
-          <h1 className="text-3xl">Simple Inputs Page</h1>
-          <FormSimpleInputs type="Create" formData={formData} />
+        <div className="flex flex-col items-center justify-center gap-4 py-10">
+          <h1 className="text-3xl">Simple Inputs with Modals</h1>
+          <div className="flex justify-center gap-2">
+            <DeleteModal
+              button={
+                <Trash
+                  size={30}
+                  className="rounded-full bg-primary stroke-background p-2"
+                />
+              }
+              id={1}
+              onDelete={() =>
+                Promise.resolve({ isSuccess: true, message: "Success" })
+              }
+            />
+            <UserModalForm
+              button={
+                <Plus
+                  size={30}
+                  className="rounded-full bg-primary stroke-background p-2"
+                />
+              }
+              type={ActionType.Create}
+              // formData={formData}
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-1 flex-col items-center justify-center gap-0">
+          {/* <h1 className="text-3xl">Simple Inputs Page</h1> */}
+          {/* <FormSimpleInputs type={ActionType.Create} formData={formData} /> */}
         </div>
       </Container>
     </section>
   );
-};
+}
