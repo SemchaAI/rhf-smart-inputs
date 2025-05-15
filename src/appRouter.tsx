a5454ee1d0ee2723cb9f21628ea0212a98d0444b
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-05-15 17:23:03 +0300

    refactor: trying to load pages lazy

--- src/appRouter.tsx
+++ src/appRouter.tsx
@@ -1,33 +1,41 @@
+import { lazy, Suspense } from "react";
+
 import { createBrowserRouter } from "react-router-dom";
 import { BaseLayout } from "./layouts/BaseLayout";
-import {
-  ContactsPage,
-  GenderFormPage,
-  HomePage,
-  LoginPage,
-  ProductAdminPage,
-  RegisterPage,
-  SimpleInputsPage,
-} from "./pages";
 import { ROUTES } from "./utils/config";
+
+const HomePage = lazy(() => import("./pages/HomePage"));
+const LoginPage = lazy(() => import("./pages/LoginPage"));
+const RegisterPage = lazy(() => import("./pages/RegisterPage"));
+const ContactsPage = lazy(() => import("./pages/ContactsPage"));
+const GenderFormPage = lazy(() => import("./pages/GenderFormPage"));
+const SimpleInputsPage = lazy(() => import("./pages/SimpleInputsPage"));
+const ProductAdminPage = lazy(() => import("./pages/ProductAdminPage"));
 
 export const appRouter = () =>
   createBrowserRouter([
     {
-      element: <BaseLayout />,
+      element: (
+        <Suspense fallback={<>loading</>}>
+          <BaseLayout />
+        </Suspense>
+      ),
       errorElement: <div>error</div>,
       children: [
         {
           path: ROUTES.HOME,
           element: <HomePage />,
+          id: "home",
         },
         {
           path: ROUTES.LOGIN,
           element: <LoginPage />,
+          id: "login",
         },
         {
           path: ROUTES.SIGNUP,
           element: <RegisterPage />,
+          id: "signup",
         },
         {
           path: ROUTES.CONTACTS,
@@ -40,6 +48,7 @@ export const appRouter = () =>
         {
           path: ROUTES.SIMPLE_INPUTS,
           element: <SimpleInputsPage />,
+          id: "simpleInputs",
         },
         {
           path: ROUTES.ADMIN_PRODUCTS,
