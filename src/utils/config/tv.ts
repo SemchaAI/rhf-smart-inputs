890ad09e1626cc25cda63ea7aeb55e560a47c3bc
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-05-15 17:15:23 +0300

    fix(ui): changed color

--- src/utils/config/tv.ts
+++ src/utils/config/tv.ts
@@ -7,14 +7,27 @@ export const button = tv({
     variant: {
       primary: "bg-primary text-text-highlight hover:bg-primary",
       secondary:
-        "bg-primary text-text-highlight hover:text-white hover:bg-primary-action",
+        "bg-primary text-text-primary hover:text-white hover:bg-primary-action",
       danger: "bg-error text-white hover:bg-error-highlight",
       outline:
         "border border-border bg-transparent text-text-highlight hover:bg-foreground",
+    },
+    rounded: {
+      none: "rounded-none",
+      sm: "rounded-sm",
+      md: "rounded-md",
+      lg: "rounded-lg",
+      full: "rounded-full",
+    },
+    size: {
+      default: "w-fit",
+      full: "w-full",
     },
   },
 
   defaultVariants: {
     variant: "primary",
+    rounded: "md",
+    size: "default",
   },
 });
