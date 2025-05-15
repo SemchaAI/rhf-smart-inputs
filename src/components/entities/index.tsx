dc2680db3db9d394b2fa6b8e9f6cc80da417a6c7
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-05-15 12:57:13 +0300

    chore: reexport

--- src/components/entities/index.tsx
+++ src/components/entities/index.tsx
@@ -3,6 +3,7 @@ export { LoginForm } from "./forms/LoginForm";
 export { RegisterForm } from "./forms/RegisterForm";
 export { FormMaskTest } from "./forms/FormMaskTest";
 export { FormContacts } from "./forms/FormContacts";
+export { FormSimpleInputs } from "./forms/FormSimpleInputs";
 export { CheckBoxRadioButtonsForm } from "./forms/CheckBoxRadioButtonsForm";
 export { ProductForm } from "./forms/ProductForm";
 
