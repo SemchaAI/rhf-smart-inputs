b774fe5c97d9cbfd552fe682347e678833a46afd
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-05-15 14:53:04 +0300

    feat(ui): add reusable DeleteModal component with confirmation logic
    mostly all delete actions need confirmation and use just one id for it. this modal will cover all this situations

---
+++ src/components/entities/modals/DeleteModal.tsx
@@ -0,0 +1,66 @@
+import { Modal } from "@/components/features";
+// import { Button } from "@/components/shared";
+import { useModal } from "@/utils/hooks";
+import { IResponse } from "@/models/response";
+// import toast from "react-hot-toast";
+
+interface IProps<T extends number | string> {
+  id: T;
+  button: React.ReactNode;
+  title?: string;
+  confirmText?: string;
+  onDelete: (id: T) => Promise<IResponse>;
+}
+
+export const DeleteModal = <T extends number | string>({
+  button,
+  title = "Delete item?",
+  confirmText = "Are you sure you want to delete this item?",
+  onDelete,
+  id,
+}: IProps<T>) => {
+  const { close, open, isOpen } = useModal();
+
+  const handleDelete = async () => {
+    try {
+      const { isSuccess, message } = await onDelete(id);
+      if (isSuccess) {
+        console.log("message", message);
+        // toast.success(message);
+        close();
+      } else {
+        console.log("message", message);
+        // toast.error(message);
+      }
+    } catch (error) {
+      console.error("[DeleteModal]", error);
+      // toast.error("Something went wrong");
+    }
+  };
+
+  return (
+    <>
+      <button
+        onClick={open}
+        className="flex cursor-pointer items-center justify-center"
+      >
+        {button}
+      </button>
+      <Modal isOpen={isOpen} onClose={close}>
+        <div className="flex flex-col gap-4">
+          <p className="text-xl font-semibold">{title}</p>
+          <p className="text-base">{confirmText}</p>
+          <button
+            className="text-secondary w-full bg-error px-2 py-1"
+            onClick={handleDelete}
+          >
+            Delete
+          </button>
+          {/* <Button variant="danger" onClick={handleDelete}>
+            Delete
+          </Button> */}
+        </div>
+      </Modal>
+    </>
+  );
+};
