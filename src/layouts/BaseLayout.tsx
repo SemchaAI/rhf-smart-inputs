dd69d08b2a9b912cb13569b9ed88d5cd999e6e4d
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-05-15 17:25:43 +0300

    refactor: changed grow class to flex-1

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -1,11 +1,11 @@
-import { Footer, Header } from '@/components/widgets';
-import { Outlet, ScrollRestoration } from 'react-router-dom';
+import { Footer, Header } from "@/components/widgets";
+import { Outlet, ScrollRestoration } from "react-router-dom";
 
 export const BaseLayout = () => {
   return (
     <>
       <Header />
-      <main className="flex flex-col grow">
+      <main className="flex flex-1 flex-col">
         <Outlet />
       </main>
       <Footer />
