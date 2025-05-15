7e2d474834cb43cd74249c744e3cb1df41ef297c
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-05-15 15:21:48 +0300

    chore: reexport

--- src/components/shared/index.tsx
+++ src/components/shared/index.tsx
@@ -3,3 +3,4 @@ export { Input } from "./input/Input";
 export { Checkbox } from "./input/Checkbox";
 export { RadioButton } from "./input/RadioButton";
 export { NavigationLink } from "./links/NavigationLink";
+export { Button } from "./buttons/Button";
