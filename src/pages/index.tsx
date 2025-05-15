61ce82e918a6219671c8356ef78bd5387a200a4a
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-05-15 17:19:14 +0300

    delete: old reexport

--- src/pages/index.tsx
+++
@@ -1,8 +0,0 @@
-export { HomePage } from "./HomePage";
-export { LoginPage } from "./LoginPage";
-export { RegisterPage } from "./RegisterPage";
-export { ContactsPage } from "./ContactsPage";
-export { GenderFormPage } from "./GenderFormPage";
-export { ProductAdminPage } from "./ProductAdminPage";
-
-export { SimpleInputsPage } from "./SimpleInputsPage";
