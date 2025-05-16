d8e667d10e7f1e758b283c7edc5ae881d0e11707
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-05-16 15:36:48 +0300

    feat: added new selects for User Form

--- src/components/entities/forms/FormSimpleInputs.tsx
+++ src/components/entities/forms/FormSimpleInputs.tsx
@@ -2,14 +2,23 @@ import { useForm } from "react-hook-form";
 import { Calendar, Mail, Phone, User } from "lucide-react";
 
 import { Form } from "./Form";
-import { FieldUnmaskedInput } from "@/components/features";
+import { FieldUnmaskedInput, SelectField } from "@/components/features";
 
 import { zodResolver } from "@hookform/resolvers/zod";
-import { type userBaseSchema, UserBaseSchema } from "@/utils/config/zod";
+import {
+  type userBaseSchema,
+  UserBaseSchema,
+  UserSex,
+} from "@/utils/config/zod";
 import type { IUserFormProps } from "@/models/zodForms";
 import { Button } from "@/components/shared";
 
-export const FormSimpleInputs = ({ formData, type }: IUserFormProps) => {
+export const FormSimpleInputs = ({
+  formData,
+  type,
+  subjectOptions,
+}: IUserFormProps) => {
+  console.log("📦📦📦FormSimpleInputs was loaded ...");
   const form = useForm({
     resolver: zodResolver(UserBaseSchema),
     defaultValues: {
@@ -18,6 +27,7 @@ export const FormSimpleInputs = ({ formData, type }: IUserFormProps) => {
       lastName: formData?.lastName,
       email: formData?.email,
       phone: formData?.phone,
+      sex: formData?.sex,
     },
     mode: "onSubmit",
   });
@@ -96,10 +106,37 @@ export const FormSimpleInputs = ({ formData, type }: IUserFormProps) => {
             Icon={Calendar}
             defaultValue={formData?.birthday?.toISOString().split("T")[0]}
           />
+          <SelectField
+            options={Object.values(UserSex).map((sex) => ({
+              label: sex.charAt(0).toUpperCase() + sex.slice(1).toLowerCase(),
+              value: sex,
+            }))}
+            name="sex"
+            label="Sex:"
+            menuPortalTarget={document.body}
+          />
+          <div className="w-full md:w-[243px]">
+            <SelectField
+              name="subjects"
+              label="Subjects:"
+              options={
+                subjectOptions?.map((subject) => ({
+                  label: subject.label,
+                  value: subject.value.toString(),
+                })) ?? []
+              }
+              isLoading={subjectOptions === undefined}
+              isDisabled={subjectOptions === undefined}
+              isMulti
+              isClearable
+              isSearchable
+              menuPortalTarget={document.body}
+            />
+          </div>
         </div>
       }
       {
-        <div className="pt-2 pb-4">
+        <div className="flex pt-2">
           <Button
             size="full"
             variant="outline"
