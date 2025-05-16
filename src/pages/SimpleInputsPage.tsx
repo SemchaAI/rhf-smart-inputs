4f8ff9f3d0031cc9ebfe30358afe316e024cc6b8
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-05-16 15:28:58 +0300

    feat: new page for demonstration new input/modal/select

--- src/pages/SimpleInputsPage.tsx
+++ src/pages/SimpleInputsPage.tsx
@@ -1,13 +1,27 @@
+import { useState } from "react";
+import { Plus, Trash } from "lucide-react";
+
 import {
   DeleteModal,
   // FormSimpleInputs,
   UserModalForm,
 } from "@/components/entities";
+import { Select } from "@/components/features";
 import { Container } from "@/components/shared";
 import { ActionType } from "@/models/zodForms";
-import { Plus, Trash } from "lucide-react";
+
+import type { IOption } from "@/models/selects";
+
+const ExampleOptions: IOption[] = [
+  { label: "Option 1", value: "option-1" },
+  { label: "Option 2", value: "option-2" },
+  { label: "Option 3", value: "option-3" },
+];
 
 export default function SimpleInputsPage() {
+  const [selectedOption, setSelectedOption] = useState<
+    IOption[] | IOption | undefined
+  >([{ label: "Option 1", value: "option-1" }]);
   // const formData = {
   //   name: "name",
   //   firstName: "firstName",
@@ -20,7 +34,7 @@ export default function SimpleInputsPage() {
     <section className="flex grow">
       <Container>
         <div className="flex flex-col items-center justify-center gap-4 py-10">
-          <h1 className="text-3xl">Simple Inputs with Modals</h1>
+          <h1 className="text-3xl">New Inputs with Modals</h1>
           <div className="flex justify-center gap-2">
             <DeleteModal
               button={
@@ -50,6 +64,31 @@ export default function SimpleInputsPage() {
         <div className="flex flex-1 flex-col items-center justify-center gap-0">
           {/* <h1 className="text-3xl">Simple Inputs Page</h1> */}
           {/* <FormSimpleInputs type={ActionType.Create} formData={formData} /> */}
+        </div>
+        <div className="flex flex-col items-center justify-center gap-2">
+          <h2 className="text-3xl">New Select Examples</h2>
+          <Select
+            options={ExampleOptions}
+            value={{ label: "Option 1", value: "option-1" }}
+            onChange={(option) => console.log(option)}
+          />
+          <Select
+            options={ExampleOptions}
+            value={selectedOption}
+            onChange={(option) => setSelectedOption(option)}
+            isMulti
+            isClearable
+            isSearchable
+            menuPortalTarget={document.body}
+            placeholder="Test placeholder..."
+          />
+          <Select
+            options={ExampleOptions}
+            value={{ label: "Option 1", value: "option-1" }}
+            onChange={(option) => console.log(option)}
+            isDisabled
+            isLoading
+          />
         </div>
       </Container>
     </section>
